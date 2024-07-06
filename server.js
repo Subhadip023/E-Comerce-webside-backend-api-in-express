a64e3a644327769fdd7815d511c36f58aeaab04f
Subhadip023 <subhadip240420@gmail.com>
2024-07-07 01:23:43 +0530

    add the docs

--- server.js
+++ server.js
@@ -1,43 +1,37 @@
 import express from 'express';
-import mongoose, { connect } from 'mongoose';
+import mongoose from 'mongoose';
 import userRoutes from './routes/userRouts.js';
 import dotenv from 'dotenv';
 import multer from "multer";
-
+import path from 'path';
 
 dotenv.config();
 
-
-
-const app =express();
-const port =5000;
+const app = express();
+const port = 5000;
 
 app.use(express.json({ extended: true }));
-
 app.use(express.urlencoded({ extended: true }));
-
 
 const storage = multer.memoryStorage(); // Use memory storage for simplicity
 const upload = multer({ storage: storage });
 app.use('/uploads', express.static('uploads')); // Serve static files from 'uploads' directory
 
-// app.use(upload.none()); // For handling form-data
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
 
-// connected to mongoDb atlas
-const uri=process.env.MONGO_URI;
-mongoose.connect(uri).then(()=>console.log('Connected to mongoAtlas')).catch(err=>console.error('Could not connect to MongoDB Atlas', err));
-
+// Connect to MongoDB Atlas
+const uri = process.env.MONGO_URI;
+mongoose.connect(uri)
+    .then(() => console.log('Connected to MongoDB Atlas'))
+    .catch(err => console.error('Could not connect to MongoDB Atlas', err));
 
 app.get('/', (req, res) => {
-    res.send('Hello this is e commerce web side backend by Subhadip and Aniket ');
-  });
+    res.render("index.ejs");
+});
 
-  
- app.use('/api/user',userRoutes);
+app.use('/api/user', userRoutes);
 
-
-
-
-app.listen(port,()=>{
-    console.log(`Server is running on port  http://localhost:${port}`)
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
 });
